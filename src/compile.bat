1ceb2b5a93f335d128e06bfbcffeca465be0734d
ZIYAD <ziyad.zerrari@gmail.com>
2026-01-05 11:15:15 +0100

    fix compile issues

--- src/compile.bat
+++ src/compile.bat
@@ -8,7 +8,7 @@ if %errorlevel% neq 0 (
 )
 
 echo Compiling C++ source and linking...
-g++ -std=c++17 -o banque.exe main.cpp adminIT.cpp caissier.cpp Client.cpp Compte.cpp CompteCourant.cpp CompteEpargne.cpp CompteManager.cpp CompteProfessionnel.cpp Employe.cpp EmployeClient.cpp InterfaceUtilisateur.cpp Manager.cpp ServiceAuthentification.cpp SystemeBancaire.cpp Transaction.cpp Utilisateur.cpp UtilisateurManager.cpp sqlite3.o -lsqlite3
+g++ -std=c++17 -o banque.exe main.cpp adminIT.cpp caissier.cpp Client.cpp Compte.cpp CompteCourant.cpp CompteEpargne.cpp CompteManager.cpp CompteProfessionnel.cpp Employe.cpp EmployeClient.cpp InterfaceUtilisateur.cpp Manager.cpp ServiceAuthentification.cpp SystemeBancaire.cpp Transaction.cpp Utilisateur.cpp UtilisateurManager.cpp sqlite3.o
 
 if %errorlevel% neq 0 (
     echo C++ compilation or linking failed.
